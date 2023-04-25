47dd48f46de82bdef108c46ef6b7229ff5478e24
justin-torok <jlt46@cornell.edu>
2023-04-25 11:39:35 -0400

    ClusterTest.m file

---
+++ Code/Transport_Network/NetworkTransportModel_Wrapper_GridSearch_ClusterTest.m
@@ -0,0 +1,104 @@
+% Wrapper script for generating output files for NetworkTransportModel for
+% generating tau distributions on the network
+
+%% 1. Define directories for saving outputs
+clear; clc;
+curpath = cd;
+p = genpath(curpath);
+addpath(p);
+simpath = [curpath filesep 'SampleFiles'];
+loadpath = [curpath filesep 'MatFiles'];
+if ~isfolder(simpath)
+    mkdir(simpath)
+end
+simstr = 'test_ECl_seed'; % for saving the outputs
+
+%% 2. Parameter definitions
+% 2a. Define actively tuned parameters as scalars or arrays to be explored
+% on a grid search
+inputparams = cell(2,8);
+paramnames = {'beta','gamma1','gamma2','frac','lambda1','lambda2',...
+    'delta','epsilon'};
+inputparams(1,:) = paramnames;
+inputparams{2,1} = 1e-6; % beta
+inputparams{2,2} = [2e-4 6e-5 2e-5]; % gamma1
+inputparams{2,3} = 0; % gamma2
+inputparams{2,4} = 0.92; % frac
+inputparams{2,5} = 0.001; % lambda1
+inputparams{2,6} = 0.001; % lambda2
+inputparams{2,7} = 1; % delta
+inputparams{2,8} = [0.01 0.5 1]; % epsilon
+
+% 2b. Create parameter array to grid search using allcomb()
+paramgrid = allcomb(inputparams{2,1},...
+                    inputparams{2,2},...
+                    inputparams{2,3},...
+                    inputparams{2,4},...
+                    inputparams{2,5},...
+                    inputparams{2,6},...
+                    inputparams{2,7},...
+                    inputparams{2,8});
+paramnamescell = repmat(paramnames,size(paramgrid,1),1);
+
+% 2c. Define other parameters
+L_int = 1000; % default = 1000; in microns
+L1 = 200; % default = 200
+L2 = 200; % default = 200
+L_ais = 40; % default = 40
+L_syn = 40; % default = 40
+T = 0.1; % default = 0.05
+dt = 0.01; % default = 0.005
+resmesh = 'coarse'; % 'fine' or 'coarse' - use 'coarse' for faster, less precise simulations
+plotting = 0;
+reltol = 1e-4;
+abstol = 1e-4;
+fsolvetol = 1e-6;
+init_path = {'Entorhinal area, lateral part_L', 'Entorhinal area, lateral part_R'};
+study = 'Hurtado';
+connectome_subset = 'Hippocampus';
+ncores = 9;
+
+%% 3. Run NetworkTransportModel
+output_struct = struct;
+output_struct.Parameter_Grid = paramgrid;   
+output_struct.Parameter_Names = inputparams(1,:);
+sim_struct = struct;
+parpool(ncores)
+tic
+% for i = 1:size(paramgrid,1)
+parfor i = 1:size(paramgrid,1)
+    fprintf('Simulation %d/%d \n',i,size(paramgrid,1))
+    paramlist = paramgrid(i,:);
+    paramnames_i = paramnamescell(i,:); % prevents broadcast warning message
+    mdloutput = NetworkTransportModel(loadpath,paramnames_i{1},paramlist(1),...
+                                paramnames_i{2},paramlist(2),...
+                                paramnames_i{3},paramlist(3),...
+                                paramnames_i{4},paramlist(4),...
+                                paramnames_i{5},paramlist(5),...
+                                paramnames_i{6},paramlist(6),...
+                                paramnames_i{7},paramlist(7),...
+                                paramnames_i{8},paramlist(8),...
+                                'L_int', L_int,...
+                                'L1', L1,...
+                                'L2', L2,...
+                                'L_ais',L_ais,...
+                                'L_syn',L_syn,...
+                                'T', T,...
+                                'dt', dt,...
+                                'resmesh', resmesh,...
+                                'plotting',plotting,...
+                                'reltol',reltol,...
+                                'abstol',abstol,...
+                                'fsolvetol',fsolvetol,...
+                                'init_path',init_path,...
+                                'study',study,...
+                                'connectome_subset',connectome_subset);
+    sim_struct(i).Model_Outputs = mdloutput;
+end
+output_struct.Simulations = sim_struct;
+delete(gcp('nocreate'));
+toc
+
+%% 4. Save output file
+save([simpath filesep simstr '.mat'],'sim_struct') 
+clear
