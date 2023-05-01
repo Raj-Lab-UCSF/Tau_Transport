85c25cfe32ab49ad4513e41422d4ffe5a637161f
justin-torok <jlt46@cornell.edu>
2023-05-01 09:57:29 -0400

    New tests for directionality to run on cluster

---
+++ Code/Transport_Network/NetworkTransport_ECl_seed_dir_9.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#### Specify job name
+#SBATCH -J Tau_Transport
+#### Output file
+#SBATCH -o "%x"_"%j".out
+#### Error file
+#SBATCH -e "%x"_"%j".err
+#### number of cores 
+#SBATCH -n 16
+#### Specify queue
+#SBATCH --partition=long
+#### --nodelist=oakland,piedmont,novato,quartzhill
+#### memory per core
+#SBATCH --mem=8G
+#### Maximum run time 
+#SBATCH --time=2-00:00:00
+
+matlab -nosplash -nodesktop -r "run('~/Tau_Transport/Code/Transport_Network/NetworkTransportModel_Wrapper_GridSearch_ECl_seed_9.m'); quit"
